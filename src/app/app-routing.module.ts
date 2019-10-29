00ec23a04a52bcd8462752d24cad089c4459f948
Gianluca Monti <gm3871@gmail.com>
2019-10-29 13:46:01 +0100

    Fix pagine per html (eliminazione elementi ionic)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,32 +2,32 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
-  { path: 'scheda-immobile', loadChildren: './scheda-immobile/scheda-immobile.module#SchedaImmobilePageModule' },
-  { path: 'report', loadChildren: './report/report.module#ReportPageModule' },
-  { path: 'analisi', loadChildren: './analisi/analisi.module#AnalisiPageModule' },
-  { path: 'nuovo-cliente', loadChildren: './nuovo-cliente/nuovo-cliente.module#NuovoClientePageModule' },
-  { path: 'scheda-cliente', loadChildren: './scheda-cliente/scheda-cliente.module#SchedaClientePageModule' },
-  { path: 'wizard', loadChildren: './wizard/wizard.module#WizardPageModule' },
-  { path: 'patrimonio', loadChildren: './patrimonio/patrimonio.module#PatrimonioPageModule' },
-  { path: 'catastali', loadChildren: './catastali/catastali.module#CatastaliPageModule' },
-  { path: 'report-generale', loadChildren: './report-generale/report-generale.module#ReportGeneralePageModule' },
-  { path: 'report-analisi', loadChildren: './report-analisi/report-analisi.module#ReportAnalisiPageModule' },
-  { path: 'ammortamento', loadChildren: './ammortamento/ammortamento.module#AmmortamentoPageModule' },
-  { path: 'wizard-destinazione', loadChildren: './wizard-destinazione/wizard-destinazione.module#WizardDestinazionePageModule' },
-  { path: 'wizard-data-destinazione', loadChildren: './wizard-data-destinazione/wizard-data-destinazione.module#WizardDataDestinazionePageModule' },
-  { path: 'wizard-dati-data', loadChildren: './wizard-dati-data/wizard-dati-data.module#WizardDatiDataPageModule' },
-  { path: 'wizard-dati-catastali', loadChildren: './wizard-dati-catastali/wizard-dati-catastali.module#WizardDatiCatastaliPageModule' },
-  { path: 'wizard-dati-tassazione', loadChildren: './wizard-dati-tassazione/wizard-dati-tassazione.module#WizardDatiTassazionePageModule' },
-  { path: 'client-home', loadChildren: './client-home/client-home.module#ClientHomePageModule' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
+    { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule' },
+    { path: 'scheda-immobile', loadChildren: './pages/scheda-immobile/scheda-immobile.module#SchedaImmobilePageModule' },
+    { path: 'report', loadChildren: './pages/report/report.module#ReportPageModule' },
+    { path: 'analisi', loadChildren: './pages/analisi/analisi.module#AnalisiPageModule' },
+    { path: 'nuovo-cliente', loadChildren: './pages/nuovo-cliente/nuovo-cliente.module#NuovoClientePageModule' },
+    { path: 'scheda-cliente', loadChildren: './pages/scheda-cliente/scheda-cliente.module#SchedaClientePageModule' },
+    { path: 'wizard', loadChildren: './pages/wizard/wizard.module#WizardPageModule' },
+    { path: 'patrimonio', loadChildren: './pages/patrimonio/patrimonio.module#PatrimonioPageModule' },
+    { path: 'catastali', loadChildren: './pages/catastali/catastali.module#CatastaliPageModule' },
+    { path: 'report-generale', loadChildren: './pages/report-generale/report-generale.module#ReportGeneralePageModule' },
+    { path: 'report-analisi', loadChildren: './pages/report-analisi/report-analisi.module#ReportAnalisiPageModule' },
+    { path: 'ammortamento', loadChildren: './pages/ammortamento/ammortamento.module#AmmortamentoPageModule' },
+    { path: 'wizard-destinazione', loadChildren: './pages/wizard-destinazione/wizard-destinazione.module#WizardDestinazionePageModule' },
+    { path: 'wizard-data-destinazione', loadChildren: './pages/wizard-data-destinazione/wizard-data-destinazione.module#WizardDataDestinazionePageModule' },
+    { path: 'wizard-dati-data', loadChildren: './pages/wizard-dati-data/wizard-dati-data.module#WizardDatiDataPageModule' },
+    { path: 'wizard-dati-catastali', loadChildren: './pages/wizard-dati-catastali/wizard-dati-catastali.module#WizardDatiCatastaliPageModule' },
+    { path: 'wizard-dati-tassazione', loadChildren: './pages/wizard-dati-tassazione/wizard-dati-tassazione.module#WizardDatiTassazionePageModule' },
+    { path: 'client-home', loadChildren: './pages/client-home/client-home.module#ClientHomePageModule' },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
