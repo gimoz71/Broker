e3d2e44892efcb3f7518adeb13b59d62ea0cd3d7
Paolo Salvadori <oloap1981@gmail.com>
2019-11-19 12:21:36 +0100

    inizio wizard

--- src/app/pages/wizard/wizard.page.ts
+++ src/app/pages/wizard/wizard.page.ts
@@ -7,7 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WizardPage implements OnInit {
 
-  constructor() { }
+  public wizardStart: boolean;
+  public wizardDestinazione: boolean;
+  public wizardDatiDestinazione: boolean;
+  public wizardDati: boolean;
+  public wizardCatastali: boolean;
+  public wizardTassazione: boolean;
+
+  constructor() {
+    this.wizardStart = true;
+    this.wizardDestinazione = false;
+    this.wizardDatiDestinazione = false;
+    this.wizardDati = false;
+    this.wizardCatastali = false;
+    this.wizardTassazione = false;
+  }
+
+  public goToDestinazione(): void {
+    this.wizardStart = false;
+    this.wizardDestinazione = true;
+    this.wizardDatiDestinazione = false;
+    this.wizardDati = false;
+    this.wizardCatastali = false;
+    this.wizardTassazione = false;
+  }
+
+  public goToDatiDestinazione(): void {
+    this.wizardStart = false;
+    this.wizardDestinazione = false;
+    this.wizardDatiDestinazione = true;
+    this.wizardDati = false;
+    this.wizardCatastali = false;
+    this.wizardTassazione = false;
+  }
+
+  public goToDati(): void {
+    this.wizardStart = false;
+    this.wizardDestinazione = false;
+    this.wizardDatiDestinazione = false;
+    this.wizardDati = true;
+    this.wizardCatastali = false;
+    this.wizardTassazione = false;
+  }
+
+  public goToCatastali(): void {
+    this.wizardStart = false;
+    this.wizardDestinazione = false;
+    this.wizardDatiDestinazione = false;
+    this.wizardDati = false;
+    this.wizardCatastali = true;
+    this.wizardTassazione = false;
+  }
+
+  public goToTassazione(): void {
+    this.wizardStart = false;
+    this.wizardDestinazione = false;
+    this.wizardDatiDestinazione = false;
+    this.wizardDati = false;
+    this.wizardCatastali = false;
+    this.wizardTassazione = true;
+  }
+
+  public goToStart(): void {
+    this.wizardStart = true;
+    this.wizardDestinazione = false;
+    this.wizardDatiDestinazione = false;
+    this.wizardDati = false;
+    this.wizardCatastali = false;
+    this.wizardTassazione = false;
+  }
 
   ngOnInit() {
   }
