6b33ae13a2d46a61c9d508a4c16866d7d26a03b2
Paolo Salvadori <oloap1981@gmail.com>
2020-01-24 20:02:09 +0100

    Sistemate date sul wizard

--- projects/broker-lib/src/lib/models/login/wsToken.ts
+++ projects/broker-lib/src/lib/models/login/wsToken.ts
@@ -1,7 +1,9 @@
 import { Utente } from './utente';
+import { Cliente } from '../clienti/cliente';
 
 export class WsToken {
     public token_value: string;
     public tipo_utente: string;
     public utente: Utente;
+    public cliente: Cliente;
 }
